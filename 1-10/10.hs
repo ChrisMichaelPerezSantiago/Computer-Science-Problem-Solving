433285933d06b02655f919372e96c16e7b316604
Chris M <chris.perez@upr.edu>
2018-06-10 14:01:04 -0400

    Update 10.hs

--- 1-10/10.hs
+++ 1-10/10.hs
@@ -1,6 +1,6 @@
 {-
     Author   : Chris M.
-    Date     : 6/9/2018
+    Date     : 6/10/2018
     Problem  : Run-length encoding of a list. Use the result of problem P09 to implement the so-called run-length
                encoding data compression method. Consecutive duplicates of elements are encoded as lists (N E) 
                where N is the number of duplicates of the element E.
